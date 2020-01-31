0ba74c2b04bc753ee1c150adc1184ec9ba8d8c74
Netanel Gilad <netanelgilad@gmail.com>
2020-01-31 17:59:28 +0200

    support for a file with a single dependency

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,67 @@
 import { ChildProcess, spawn } from 'child_process';
-import { transformFileAsync } from '@babel/core';
+import { parseAsync, transformFromAstAsync, NodePath } from '@babel/core';
 import { file } from 'tempy';
-import { writeFileSync } from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
+import {
+  File,
+  isImportDeclaration,
+  ImportDeclaration,
+  stringLiteral,
+} from '@babel/types';
+import { resolve, dirname } from 'path';
 
-export async function runFile(path: string): Promise<ChildProcess> {
-  const { code } = (await transformFileAsync(path, {
+export async function buildFile(path: string): Promise<string> {
+  const fileContents = readFileSync(path, 'utf8');
+  const ast = await parseAsync(fileContents, {
+    filename: path,
     presets: ['@babel/preset-typescript'],
+  });
+
+  const importDeclarations: ImportDeclaration[] = (((ast as unknown) as File).program.body.filter(
+    statement => isImportDeclaration(statement)
+  ) as unknown) as ImportDeclaration[];
+
+  const dependenciesToOutputFiles = new Map();
+
+  for (const dependency of importDeclarations) {
+    const dependencyPath = dependency.source.value;
+    const dependencyOutputFile = await buildFile(
+      resolve(dirname(path), dependencyPath)
+    );
+    dependenciesToOutputFiles.set(dependencyPath, dependencyOutputFile);
+  }
+
+  const { code } = (await transformFromAstAsync(ast!, fileContents, {
+    filename: path,
+    presets: [
+      '@babel/preset-typescript',
+      [
+        '@babel/preset-env',
+        {
+          targets: ['current node'],
+        },
+      ],
+    ],
+    plugins: [
+      () => ({
+        visitor: {
+          ImportDeclaration(path: NodePath<ImportDeclaration>) {
+            path.node.source = stringLiteral(
+              dependenciesToOutputFiles.get(path.node.source.value)
+            );
+          },
+        },
+      }),
+    ],
   }))!;
+
   const tmpFilePath = file();
   writeFileSync(tmpFilePath, code);
+  return tmpFilePath;
+}
 
-  return spawn('node', [tmpFilePath]);
+export async function runFile(path: string): Promise<ChildProcess> {
+  const outputFile = await buildFile(path);
+
+  return spawn('node', [outputFile]);
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,14 @@
-import { file } from 'tempy';
+import { file, directory } from 'tempy';
 import { writeFileSync, unlinkSync } from 'fs';
 import { runFile } from '../src/index';
 import { Chance } from 'chance';
+import { join } from 'path';
 
 describe('runFile', () => {
   it('should run a typecript file', async () => {
     const tmpFilePath = file({ extension: 'ts' });
     const chance = new Chance();
-    const expectedStdout = `Hello world ${chance.string()}`;
+    const expectedStdout = chance.string();
 
     writeFileSync(
       tmpFilePath,
@@ -31,5 +32,48 @@ describe('runFile', () => {
     } finally {
       unlinkSync(tmpFilePath);
     }
+  });
+
+  describe('with dependencies', () => {
+    it('should run a file with a single dependency', async () => {
+      const tmpDirectory = directory();
+      const dependantFilePath = join(tmpDirectory, 'dependant.ts');
+      const dependencyFilePath = join(tmpDirectory, 'dependency.ts');
+      const chance = new Chance();
+      const expectedStdout = chance.string();
+
+      writeFileSync(
+        dependantFilePath,
+        `
+        import {foo} from "./dependency.ts";
+        foo();
+      `
+      );
+
+      writeFileSync(
+        dependencyFilePath,
+        `
+        export function foo() {
+          console.log('${expectedStdout}');
+        }
+      `
+      );
+
+      try {
+        const childProcess = await runFile(dependantFilePath);
+
+        expect(childProcess.stdout).toBeDefined();
+
+        let output = '';
+        for await (const chunk of childProcess.stdout!) {
+          output += chunk;
+        }
+
+        expect(output).toEqual(expectedStdout + '\n');
+      } finally {
+        unlinkSync(dependantFilePath);
+        unlinkSync(dependencyFilePath);
+      }
+    });
   });
 });
